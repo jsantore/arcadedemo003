1d6f8d97e8d09e236f263f335201426de90923fd
jsantore <jsantore@bridgew.edu>
2022-09-29 11:52:22 -0400

    added triangle and circle

--- shapesAllOver.py
+++ shapesAllOver.py
@@ -9,7 +9,10 @@ arcade.draw_lrtb_rectangle_filled(320, 420, 300, 50, arcade.color.ELECTRIC_INDIG
 arcade.draw_lrtb_rectangle_outline(450, 550, 300, 50, arcade.color.BRINK_PINK, 12)
 arcade.draw_xywh_rectangle_filled(380, 120, 300, 80, arcade.color.AQUAMARINE)
 arcade.draw_xywh_rectangle_outline(10, 10, 680, 300, arcade.color.BRIGHT_CERULEAN, 8)
-
+arcade.draw_circle_filled(150, 600, 75, arcade.color.CORDOVAN)
+arcade.draw_circle_outline(150, 600, 125, arcade.color.BANANA_MANIA , 5)
+arcade.draw_triangle_filled(300, 600, 600, 600, 450, 350, arcade.color.BITTERSWEET_SHIMMER)
+arcade.draw_triangle_outline(600, 600, 490, 300, 700, 700, arcade.color.JADE, 8)
 
 arcade.finish_render()
 
